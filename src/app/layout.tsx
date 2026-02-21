d93bab09de1eb09fc70d9811d44da1d09cc78347
morveusz93 <morveusz93@gmail.com>
2026-02-21 09:09:54 +0100

    feat: favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,21 +4,27 @@ import "./globals.css";
 export const metadata: Metadata = {
   metadataBase: new URL("https://morv.dev"),
   title: {
-    default: "Morv (DreamSoft) | Strony internetowe dla firm",
-    template: "%s | Morv (DreamSoft)",
+    default: "Morv | Strony internetowe dla firm",
+    template: "%s | Morv",
   },
   description:
     "Projektuję szybkie i skuteczne strony internetowe dla małych firm. Landing page, strony firmowe, modernizacja i SEO.",
   openGraph: {
     type: "website",
     locale: "pl_PL",
-    title: "Morv (DreamSoft) | Strony internetowe dla firm",
+    title: "Morv | Strony internetowe dla firm",
     description: "Profesjonalna strona w zasięgu Twojej ręki.",
     url: "https://morv.dev",
-    siteName: "Morv (DreamSoft)",
+    siteName: "Morv",
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: [
+      { url: "/favicon/favicon.ico" },
+      { url: "/favicon/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon/apple-touch-icon.png", sizes: "180x180" }],
+    other: [{ rel: "manifest", url: "/favicon/site.webmanifest" }],
   },
 };
 
